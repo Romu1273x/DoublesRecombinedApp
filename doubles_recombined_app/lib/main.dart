fb2d1be36bc20918270b1d9b1b62c41f672b9237
Romu1273x <romu1273x@yahoo.co.jp>
2022-04-13 08:53:23 +0900

    Providerの定義箇所を変更

--- doubles_recombined_app/lib/main.dart
+++ doubles_recombined_app/lib/main.dart
@@ -1,42 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:doubles_recombined_app/view/common_view.dart';
+import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
 import 'package:doubles_recombined_app/view_model/common_view_model.dart';
 import 'package:doubles_recombined_app/view_model/user_view_model.dart';
 import 'package:doubles_recombined_app/view_model/participant_view_model.dart';
-
-// TODO:消す。レイアウトデバック機能
-import 'package:flutter/rendering.dart';
+import 'package:doubles_recombined_app/view/common_view.dart';
 
 void main() {
-  // TODO:消す。レイアウトデバック機能
-  //debugPaintSizeEnabled = true;
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // 状態管理をするモデルの設定
-    return MultiProvider(
+  debugPaintSizeEnabled = false; // レイアウトデバック機能
+  runApp(
+		MultiProvider( // providerの定義
       providers: [
         ChangeNotifierProvider<CommonViewModel>(create: (context) => CommonViewModel()),
         ChangeNotifierProvider<UserViewModel>(create: (context) => UserViewModel()),
         ChangeNotifierProvider<ParticipantViewModel>(create: (context) => ParticipantViewModel()),
       ],
-      child: MaterialApp(
-        // TODO:消す。レイアウトデバック機能
-        //debugShowMaterialGrid: true,
-        // テーマ（仮）
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        // ルーティングの設定
-        initialRoute: '/home',
-        routes: {
-          '/home': (context) => CommonView(), 
-        },
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowMaterialGrid: false, // レイアウトデバック機能
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      // ルーティングの設定
+      initialRoute: '/home',
+      routes: {
+        '/home': (context) => CommonView(), 
+      },
     );
   }
 }
